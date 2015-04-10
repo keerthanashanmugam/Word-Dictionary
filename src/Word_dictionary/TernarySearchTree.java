4355965d1861e8570fffdafea6c5ebef1138c34e
keerthanashanmugam <keerthismart555@gmail.com>
2015-04-10 17:10:00 +0530

    added search and autocomplete

--- src/Word_dictionary/TernarySearchTree.java
+++ src/Word_dictionary/TernarySearchTree.java
@@ -1,4 +1,5 @@
 package Word_dictionary;
+import java.util.ArrayList;
 
 public class TernarySearchTree {
 	Node root;
@@ -30,6 +31,41 @@ public class TernarySearchTree {
 				}
 			}
 		}
+		
+	}
+	
+	public Node search(Node n,String word,int index){
+		
+		if(n==null)
+			return null;
+		else if(word.charAt(index)<n.key)
+			return this.search(n.highchild, word, index);
+		else if(word.charAt(index)>n.key)
+			return this.search(n.lochild, word, index);
+		else
+		{
+			if(index == word.length()-1)
+				return n;
+			return this.search(n.equalchild, word, index+1);
+		}
+	}
+	
+	public void getListFrom(Node n,ArrayList<String> lst,String s){
+		
+		if(n == null)
+			lst.add(s);
+		getListFrom(n.lochild,lst,s);
+		getListFrom(n.equalchild,lst,s+n.key);
+		getListFrom(n.highchild,lst,s);
+		
+	}
+	
+	public ArrayList<String> autoComplete(String word,int index){
+		
+		Node n = this.search(root, word, 0);	
+		ArrayList<String> lst = new ArrayList<String>();
+		getListFrom(n,lst,word);
+		return lst;
 		
 	}
 	
